f8747b289559ac871404809259b04fc49245f11e
Adriaan Jacobs <adriaan.jacobs@kuleuven.be>
2021-11-08 20:48:20 +0100

    why does SVF modify the module, why why why

--- util.h
+++ util.h
@@ -1,10 +1,12 @@
+#pragma once
+
+#if 0
 #ifndef BOOST_STACKTRACE_USE_BACKTRACE
     #define BOOST_STACKTRACE_USE_BACKTRACE
 #endif
 
 #include <boost/stacktrace.hpp>
 
-#include <functional>
 #include <source_location>
 #include <llvm/Support/raw_ostream.h>
 #include <signal.h>
@@ -21,9 +23,34 @@
         std::cerr << std::endl; \
         raise(SIGABRT); \
     }
+#endif
+
+#include <functional>
+#include <llvm/IR/Operator.h>
 
 struct run_on_destruct {
     std::function<void()> func;
     run_on_destruct(auto func) : func{std::move(func)} {}
     ~run_on_destruct() { func(); }
 };
+
+#define HANDLE_UNKOWN_VALUE(val)                                    \
+    do {        \
+        llvm::outs() << "Unkown value type: \n";      \
+        llvm::outs() << "\t" << *val << "\n\n";     \
+        llvm::outs() << "Is constant: " << (llvm::isa<llvm::Constant>(val) ? "yes" : "no") << "\n"; \
+        llvm::outs() << "Is GlobalVariable: " << (llvm::isa<llvm::GlobalVariable>(val) ? "yes" : "no") << "\n"; \
+        llvm::outs() << "Is ConstantData: " << (llvm::isa<llvm::ConstantData>(val) ? "yes" : "no") << "\n"; \
+        llvm::outs() << "Is instruction: " << (llvm::isa<llvm::Instruction>(val) ? "yes" : "no") << "\n"; \
+        llvm::outs() << "Is operator: " << (llvm::isa<llvm::Operator>(val) ? "yes" : "no") << "\n"; \
+        assert(!"Unkown instruction!");     \
+    } while (false)
+
+#define ASSERT_ELSE_UNKOWN(cond, val) \
+    do {                                                \
+        bool condVal = static_cast<bool>(cond);     \
+        if (!condVal) {                                \
+            HANDLE_UNKOWN_VALUE(val);               \
+        }                                           \
+    } while (false)
+
