db1b99f5dbb70bc71187fba397de3b9bd917b105
Adriaan Jacobs <adriaan.jacobs@kuleuven.be>
2023-09-06 22:16:50 +0200

    compileOTF: factor into separate directory

---
+++ compileOTF/compileOTF.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "util.h"
+#include <clang/Basic/LangStandard.h>
+
+
+std::unique_ptr<llvm::Module> compileIntoBitcode(std::string_view path, clang::Language lang, std::string_view targetTriple, llvm::LLVMContext* context);
+llvm::DenseSet<llvm::Function*> compileAndLinkIntoModule(std::string_view code, llvm::Module& module);
+
+
