b7065af66b3b22b710d64bd1761988c3dad2c7a8
Adriaan Jacobs <adriaan.jacobs@kuleuven.be>
2024-10-29 11:35:18 +0100

    add a wrapping/internalization transform for address-taken functions
    this enables invasive transformations like signature changes on many internal functions

--- include/llvm-utils/addressability/addressability.h
+++ include/llvm-utils/addressability/addressability.h
@@ -7,3 +7,8 @@
 //  mostly used to prune out safe stack allocations
 bool ptrMayReachUnsafeAccesses(llvm::Value* ptr, const UnsafeAccessInfo& unsafeAccessInfo, const CallSiteAnalysisResult& callSiteAnalysis);
 
+// internalize a bunch of functions that may be called indirectly/from external code
+//  according to CallSiteAnalysis
+// returns a mapping between the inserted wrapper function and the wrapped ("internalized") function
+// the "internalized" functions are suitable for invasive transformations like signature changes etc.
+llvm::DenseMap<llvm::Function*, llvm::Function*> wrapAddressTakenFuncs(llvm::Module& module, llvm::ModuleAnalysisManager& MAM);
